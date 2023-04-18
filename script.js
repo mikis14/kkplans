9aac6153bfaa5377bc6a1a334c1b45f1291ed55c
Marika <97937577+mikis14@users.noreply.github.com>
2023-04-18 08:41:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const plusins = document.querySelector('.js-plus');
-const multiply = document.querySelector('.js-multiply')
-const minus = document.querySelector('.js-minus')
-const divide = document.querySelector('.js-divide')
-const inputins1 = document.querySelector('.js-input1')
-const inputins2 = document.querySelector('.js-input2')
+const multiply = document.querySelector('.js-multiply');
+const minus = document.querySelector('.js-minus');
+const divide = document.querySelector('.js-divide');
+const inputins1 = document.querySelector('.js-input1');
+const inputins2 = document.querySelector('.js-input2');
 const answer = document.querySelector('.js-result');
-
-let firstValue = inputins1.value
-let secondValue = inputins2.value
 
 const Add = (cipars1, cipars2) => {
     return cipars1 + cipars2
@@ -19,15 +16,46 @@ const Subtract = (cipars1, cipars2) => {
 
 const Multiply = (cipars1, cipars2) => {
     return cipars1 * cipars2
-} 
+}
 
 const Divide = (cipars1, cipars2) => {
     return cipars1 / cipars2
 }
 
-plusins.addEventListener('click', () => {
-    answer.textContent = Add(parseInt(inputins1.value) ,parseInt(inputins2.value))
-
+const clear = () => {
     inputins1.value = ""
     inputins2.value = ""
+}
+
+plusins.addEventListener('click', () => {
+    answer.textContent = Add
+    (parseInt(inputins1.value),
+    parseInt(inputins2.value))
+
+    clear()
 })
+
+minus.addEventListener('click', () => {
+    answer.textContent = Subtract
+    (parseInt(inputins1.value),
+    parseInt(inputins2.value))
+
+    clear()
+})
+
+multiply.addEventListener('click', () => {
+    answer.textContent = Multiply
+    (parseInt(inputins1.value),
+    parseInt(inputins2.value))
+
+    clear()
+})
+
+divide.addEventListener('click', () => {
+    answer.textContent = Divide
+    (parseInt(inputins1.value),
+    parseInt(inputins2.value))
+
+    clear()
+})
+
